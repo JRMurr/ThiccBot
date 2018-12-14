d5604e1dd23018aa5e77f5122b9df46af441b40e
John Murray <5672686+JRMurr@users.noreply.github.com>
2018-12-14 12:48:01 -0500

    move to rest, db wont init

--- backend/src/database/discordServer.py
+++
@@ -1,7 +0,0 @@
-from src.database.base import Base
-from sqlalchemy import *
-
-class DiscordServer(Base):
-    __tablename__ = 'discordserver'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
