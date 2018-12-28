73911aa70cb1cfa5d73b9ba8f588830f7731c46a
John Murray <5672686+JRMurr@users.noreply.github.com>
2018-12-28 00:31:29 -0500

    add many commadns

--- backend/src/models/alias.py
+++ backend/src/models/alias.py
@@ -1,9 +1,10 @@
 from src import db
+from sqlalchemy.dialects.postgresql import BIGINT
 
 class Alias(db.Model):
     __tablename__ = 'alias'
     id = db.Column(db.Integer, primary_key=True)
-    server_id = db.Column(db.Integer, db.ForeignKey('discordserver.id'))
+    server_id = db.Column(BIGINT, db.ForeignKey('discordserver.id'))
     name = db.Column(db.String, doc="name of the alias")
     command = db.Column(db.String, doc="the command with args that the alias maps to")
 
--- bot/thiccBot/bot.py
+++ bot/thiccBot/bot.py
@@ -4,11 +4,21 @@ import traceback
 import sys
 from pprint import pprint
 import logging
-from thiccBot.cogs.utils.requestHelp import requestHelp
+import aiohttp
+import asyncio
+# from thiccBot.cogs.utils.requestHelp import requestHelp
+import os
+
+BACKEND_URL = os.environ['BACKEND_URL']
+if not BACKEND_URL.endswith('/'):
+    BACKEND_URL += '/'
+
+
 description = '''THICC BOI'''
 log = logging.getLogger(__name__)
 
-def _prefix_callable(bot, msg, config):
+def _prefix_callable(bot, msg):
+    config = bot.config
     user_id = bot.user.id
     base = [f'<@!{user_id}> ', f'<@{user_id}> ']
     if msg.guild is None:
@@ -19,10 +29,11 @@ def _prefix_callable(bot, msg, config):
 
 class ThiccBot(commands.Bot):
     def __init__(self, config):
-        super().__init__(command_prefix=(lambda bot, msg: _prefix_callable(bot, msg, config)),
-                        description=description, pm_help=None, owner_id=config['bot_admin'])
+        super().__init__(command_prefix=_prefix_callable, description=description,
+                            pm_help=None, owner_id= config['bot_admin'])
         self.config = config
         self.prefixes = {}
+        self.session = aiohttp.ClientSession(loop=self.loop)
         # self.add_command(self.do)
         for extension in config['initial_extensions']:
             try:
@@ -31,19 +42,38 @@ class ThiccBot(commands.Bot):
                 print(f'Failed to load extension {extension}.', file=sys.stderr)
                 traceback.print_exc()
 
-    def addGuild(self, guild):
-        r = requestHelp.post('/servers', data={'name': guild.name, 'id': guild.id})
-        data = r.json()
-        if 'command_prefix' in data:
-            # TODO: check if command prefix is list and make it one if not
-            self.prefixes[data['id']] = data['command_prefix'] 
-            log.info(f'server: {data['name']}({data['id']}), has command_prefix: {data['command_prefix']}')
+    def get_command_prefixes(self, msg):
+        return _prefix_callable(self, msg)
+
+    def backend_request(self, method: str, api_end_point: str, **kwargs) -> aiohttp.ClientResponse:
+        '''prepends backend server base url to an an api request'''
+        if api_end_point.startswith('/'):
+            api_end_point = api_end_point[1:]
+        url = BACKEND_URL + api_end_point
+        # TODO: auth stuff
+        return self.session.request(method, url, **kwargs)
+
+    async def add_guild(self, guild):
+        async with self.backend_request('post', '/servers', data={'name': guild.name, 'id': guild.id}) as r:
+            data = await r.json()
+            if 'command_prefix' in data and data['command_prefix'] is not None:
+                # TODO: check if command prefix is list and make it one if not
+                self.prefixes[data['id']] = data['command_prefix'] 
+                log.info('server: %s, id: %s, has command_prefix: %s', data['name'], data['id'], data['command_prefix'])
 
     async def on_ready(self):
         for guild in self.guilds:
-            self.addGuild(guild)
+            await self.add_guild(guild)
         print('Logged on as {0}!'.format(self.user))
 
+    async def close(self):
+        await super().close()
+        await self.session.close()
+
     async def on_message(self, message):
-        # print('Message from {0.author}: {0.content}'.format(message))
         await self.process_commands(message)
+        # try:
+        #     await self.process_commands(message)
+        # except commands.CommandNotFound:
+        #     # swallow not found errors since all aliases are not 'real' commands
+        #     pass
