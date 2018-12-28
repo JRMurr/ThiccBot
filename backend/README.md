5715d2e079994067213c6def40979a92b54be32b
John Murray <johnreillymurray@gmail.com>
2018-12-28 00:31:29 -0500

    add many commadns

--- backend/README.md
+++ backend/README.md
@@ -6,3 +6,8 @@
 
 # Dev
 - if you need to print something to console use `app.logger.warning`, `app.logger.error`, or `app.logger.info`
+
+# vscode pylint
+To get rid of errors about the sql models do the following
+1. `pip install pylint-flask` in your conda env
+2. add `"python.linting.pylintArgs": ["--load-plugins", "pylint_flask"]` to your settings file
--- bot/thiccBot/cogs/alias.py
+++ bot/thiccBot/cogs/alias.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
-from thiccBot.cogs.utils.requestHelp import requestHelp
+import discord
 import logging
+from pprint import pprint
+from copy import copy
 log = logging.getLogger(__name__)
 
 
@@ -8,10 +10,30 @@ class Alias:
     def __init__(self, bot):
         self.bot = bot
     
-    async def on_message(self, message):
+    async def get_alias_command(self, message: discord.Message):
+        if message.guild is None:
+            return
+        for prefix in self.bot.get_command_prefixes(message):
+            if message.content.startswith(prefix):
+                alias_name = message.content[len(prefix):]
+                if alias_name in self.bot.commands:
+                    continue
+                server_id = message.guild.id
+                async with self.bot.backend_request('get', f'/alias/{server_id}/{alias_name}') as r:
+                    if r.status == 200:
+                        data = await r.json()
+                        msg = copy(message)
+                        msg.content = prefix + data['command']
+                        await self.bot.process_commands(msg)
+                    elif r.status == 403:
+                        log.error('error making alias get request') #TODO: error info
+                break
+
+    async def on_message(self, message: discord.Message):
         author = message.author
         if author.id == self.bot.user.id:
             return
+        alias_info = await self.get_alias_command(message)
 
 
     # @commands.is_owner()
@@ -24,17 +46,14 @@ class Alias:
         if name in self.bot.commands or name is "help":
             await ctx.send("dont overwrite big boi commands")
         else:
+            pass
             server_id = ctx.guild.id
-            r = requestHelp.post('/alias', data={'name': name, 'args': args, 'server_id': server_id})
-            if r.status_code == 200:
-                await ctx.send("Created alias " + name)
-            else:
-                await ctx.send("Error creating alias")
-                log.error('error sending request to server : ') #TODO: error msg
-
-    @commands.command()
-    async def say(self, ctx, *, args: str):
-        await ctx.send(args)
+            async with self.bot.backend_request('post','/alias', json={'name': name, 'command': args, 'server_id': server_id}) as r:
+                if r.status == 200:
+                    await ctx.send("Created alias " + name)
+                else:
+                    await ctx.send("Error creating alias")
+                    log.error('error sending request to server : ') #TODO: error msg
 
 
 def setup(bot):
