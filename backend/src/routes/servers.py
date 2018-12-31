8a6d643ee1b4c85ce70a7dbcb90b385e139944e7
John Murray <5672686+JRMurr@users.noreply.github.com>
2018-12-30 19:46:45 -0500

    owner of server can set the bot admin role

--- backend/src/routes/servers.py
+++ backend/src/routes/servers.py
@@ -1,24 +1,33 @@
 from src import app
-from flask import request
+from flask import request, abort
 from pprint import pformat
 from src import db
 from src.models import DiscordServer
 from flask import jsonify
 
 
-@app.route("/servers", methods=['POST', 'GET'])
+@app.route("/servers", methods=['POST'])
 def serverRoute():
-    if request.method == 'POST':
-        form = request.form
-        serverId = int(form['id'])
-        serverName = form['name']
-        server = DiscordServer.query.get(serverId)
-        if server is None:
-            server = DiscordServer(id=serverId, name=serverName)
-            db.session.add(server)
-            db.session.commit()
-            app.logger.info(f'added server: {server}')
-        return jsonify(server.serialize)
+    form = request.form
+    serverId = int(form['id'])
+    serverName = form['name']
+    server = DiscordServer.query.get(serverId)
+    if server is None:
+        server = DiscordServer(id=serverId, name=serverName)
+        db.session.add(server)
+        db.session.commit()
+        app.logger.info(f'added server: {server}')
     else:
-        # TODO: query with search params?
-        return 'big doinks'
+        app.logger.info(f'already had server: {server}')
+    return jsonify(server.serialize)
+
+@app.route('/servers/<int:server_id>', methods=['PUT', 'GET'])
+def getServer(server_id):
+    server = DiscordServer.query.get_or_404(server_id)
+    if request.method == 'PUT':
+        server = DiscordServer.query.get_or_404(server_id)
+        form = request.get_json()
+        if form['admin_role']:
+            server.admin_role = form['admin_role']
+        db.session.commit()        
+    return jsonify(server.serialize)
---
+++ bot/thiccBot/cogs/admin.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+import discord
+from  thiccBot.cogs.utils import checks
+import logging
+from pprint import pprint
+log = logging.getLogger(__name__)
+
+
+class Admin:
+    '''Admin commands to manage the bot in each server'''
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.is_server_owner()
+    async def set_bot_admin(self, ctx, role_name: str):
+        '''Sets the bot admin role in this server
+
+        Users with this role can run commands to edit the bots config for this server'''
+        guild = ctx.message.guild
+        server_roles = guild.roles
+        desired_role = next((x for x in server_roles if x.name == role_name), None)
+        if desired_role is None:
+            await ctx.send(f'role with name ({role_name}) not found, make sure spelling and capitilization are the same')
+            return
+        async with self.bot.backend_request('put', f'/servers/{guild.id}', json={'admin_role': desired_role.id}) as r:
+            if r.status == 200:
+                await ctx.send(f'set the bot admin role to {desired_role.name}, any one who has this role or one above it can run any admin only command')
+            else:
+                await ctx.send(f'error setting admin role')
+                #TODO: log error
+    
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
