13adfb4a6867ad18d6e5001006699a0f53b19f6a
John Murray <5672686+JRMurr@users.noreply.github.com>
2021-11-07 23:55:36 -0500

    add guild get and create

--- rust_bot/crates/client/src/guilds.rs
+++ rust_bot/crates/client/src/guilds.rs
@@ -5,16 +5,30 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DiscordGuild {
+    /// the name of the server/guild
     name: String,
+    /// the guild id
+    id: u64,
+    /// The id of the guild in the backend
+    server_group_id: u64,
+
+    admin_role: Option<u64>,
+
+    // TODO: check if these need to be options, the server might send an empty array
     command_prefixes: Vec<String>,
     message_prefixes: Vec<String>,
-    server_group_id: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DiscordGuildCreate {
+    /// the name of the server/guild
+    name: String,
+    /// the guild id
     id: u64,
-    admin_role: u64,
 }
 
 impl ThiccClient {
-    pub async fn get_guild(&self, guild_id: &str) -> Result<Option<DiscordGuild>> {
+    pub async fn get_guild(&self, guild_id: u64) -> Result<Option<DiscordGuild>> {
         let res = self.get(&format!("/discord/{}", guild_id))?.send().await?;
         match res.error_for_status() {
             Ok(response) => Ok(Some(response.json::<DiscordGuild>().await?)),
@@ -28,5 +42,19 @@ impl ThiccClient {
         }
     }
 
-    // pub async fn create_guild(&self, guild)
+    pub async fn create_guild(&self, guild_id: u64, name: &str) -> Result<DiscordGuild> {
+        let payload = DiscordGuildCreate {
+            id: guild_id,
+            name: name.to_string(),
+        };
+        let res = self
+            .post("/discord")?
+            .json(&payload)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await?;
+        Ok(res)
+    }
 }
