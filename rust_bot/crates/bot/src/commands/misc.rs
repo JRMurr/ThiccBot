703bba6370367c3e57eda0d4fffbdf3ab70e9b66
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-03-13 18:18:44 -0400

    add choose commands

--- rust_bot/crates/bot/src/commands/misc.rs
+++ rust_bot/crates/bot/src/commands/misc.rs
@@ -1,3 +1,4 @@
+use rand::seq::{IteratorRandom, SliceRandom};
 use serenity::{
     client::Context,
     framework::standard::{
@@ -8,12 +9,40 @@ use serenity::{
 };
 
 #[group]
-#[commands(say)]
+#[commands(say, choose, choose_list)]
 pub struct Misc;
 
 #[command]
 async fn say(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     msg.channel_id.say(&ctx.http, args.rest()).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn choose(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let answer = args
+        .raw_quoted()
+        .choose(&mut rand::thread_rng())
+        .unwrap_or("No options passed");
+    msg.channel_id.say(&ctx.http, answer).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn choose_list(
+    ctx: &Context,
+    msg: &Message,
+    args: Args,
+) -> CommandResult {
+    let mut list: Vec<_> = args.raw_quoted().collect();
+    list.shuffle(&mut rand::thread_rng());
+
+    // TODO: make this look nicer
+    let res = format!("{:?}", list);
+
+    msg.channel_id.say(&ctx.http, res).await?;
 
     Ok(())
 }
