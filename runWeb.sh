410dc91d51b2b65bd0bf4430b582b504101f9f78
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-03-26 16:24:55 -0400

    yeet web

--- runWeb.sh
+++
@@ -1,5 +0,0 @@
-# runs a local web instance instead of in docker
-pushd web/
-npm run build
-PORT=4000 npm run dev
-popd
--- web/clean.sh
+++
@@ -1,5 +0,0 @@
-# Using this to clean out all compiled stuff to easily switch from docker to local compile
-rm -rf ./lib
-rm -rf ./node_modules
-rm .bsb.lock
-rm .merlin
