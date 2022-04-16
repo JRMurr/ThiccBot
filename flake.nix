77d54eda694b48f269132c19ae4bf0b416e3735e
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-04-16 13:59:36 -0400

    use flakes

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = ((pkgs.rust-bin.fromRustupToolchainFile
+          ./rust-toolchain.toml).override { extensions = [ "rust-src" ]; });
+      in with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            rustVersion
+
+            cargo-expand
+            gcc
+            cmake
+            # openssl
+            zlib
+            pkgconfig
+
+            #python
+            python38
+            python38Packages.virtualenv
+
+            # common
+            watchexec
+
+            nixfmt
+          ];
+
+          shellHook = ''
+            alias ls=exa
+            alias find=fd
+          '';
+        };
+      });
+}
